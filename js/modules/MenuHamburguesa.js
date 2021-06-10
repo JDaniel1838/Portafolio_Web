d7075f4061584be4cd40cd5a4e0b55d8b6b6af33
unknown <j.danielesp8062@gmail.com>
2021-06-10 15:29:46 -0500

    Responsive 80%

--- js/modules/MenuHamburguesa.js
+++ js/modules/MenuHamburguesa.js
@@ -9,19 +9,9 @@ export default function MenuHamburguesa() {
             
             Display.classList.add("activo");
             Activo = true;
-            Display.classList.add("animate__fadeInRight");
-            setInterval(() => {
-                Display.classList.remove("animate__fadeInRight");
-            }, 1000);
-        } else {
             
-
-            Display.classList.add("animate__fadeOutRight");
-
-            setTimeout(() => {
-                Display.classList.remove("activo");
-                Display.classList.remove("animate__fadeOutRight");
-            }, 1000);
+        } else {
+            Display.classList.remove("activo");
             
             Activo = false;
         }
